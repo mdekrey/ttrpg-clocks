52ee5d3e24f650ba482960bee373e6218d8f12dd
Matt DeKrey <mattdekrey@gmail.com>
2021-01-19 07:13:24 -0600

    Correct random game/gamer id

--- ttrpg-clock-ui/src/pages/index.tsx
+++ ttrpg-clock-ui/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from "react";
 import { v4 as uuid } from "uuid";
+import { isSSR } from "../utils/isSSR";
 
 export default () => {
 	const gameId = useMemo(uuid, []);
@@ -11,12 +12,16 @@ export default () => {
 			<h1>TTRP Clock Tracker</h1>
 			<p>
 				Use a URL sent to you by your GM (or saved from before) or{" "}
-				<a
-					href={`/clocks?game=${gameId}&user=${gamerId}`}
-					className="bg-gray-300 border border-gray-800 rounded p-1 shadow"
-				>
-					Start new Clocks
-				</a>
+				{isSSR() ? (
+					"Start new Clocks"
+				) : (
+					<a
+						href={`/clocks?game=${gameId}&user=${gamerId}`}
+						className="bg-gray-300 border border-gray-800 rounded p-1 shadow"
+					>
+						Start new Clocks
+					</a>
+				)}
 				.
 			</p>
 			<div className="flex-grow"></div>
